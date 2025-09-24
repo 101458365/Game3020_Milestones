ba12beb0c01d926e523ba573889faed93288f065
101458365 <bobalan47@gmail.com>
2025-09-24 12:57:32 -0400

    added a attack for my player
    so i decided instead of doing camera work like is said i was going to do in my previous commit i thought it was a good idea to add an attack option. I also added an enemy that does absolutely nothing atm

--- Game3020/Assets/Scripts/PlayerControls.cs
+++ Game3020/Assets/Scripts/PlayerControls.cs
@@ -8,20 +8,24 @@ public class PlayerControls : MonoBehaviour
     public float jumpForce = 10f;
     public float rotationSpeed = 180f;
 
+    [Header("Combat Settings")]
+    public float attackRange = 2f;
+    public float attackCooldown = 1f;
+    public GameObject attackEffect;
+
     private Rigidbody rb;
     public bool isGrounded;
     private Vector2 moveInput;
+    private float lastAttackTime;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
-        
     }
 
     void Update()
     {
-        
+
     }
 
     void FixedUpdate()
@@ -32,16 +36,32 @@ public class PlayerControls : MonoBehaviour
     void Move()
     {
         Vector3 direction = new Vector3(moveInput.x, 0, moveInput.y).normalized;
-
         if (direction.magnitude >= 0.1f)
         {
             Vector3 moveDirection = direction * moveSpeed * Time.fixedDeltaTime;
             rb.MovePosition(transform.position + moveDirection);
-
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, rotationSpeed * Time.fixedDeltaTime);
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
         }
+    }
+
+    void PerformMeleeAttack()
+    {
+        if (Time.time < lastAttackTime + attackCooldown)
+            return;
+
+        lastAttackTime = Time.time;
+
+        if (attackEffect != null)
+        {
+            Vector3 effectPosition = transform.position + transform.forward * attackRange;
+            GameObject effect = Instantiate(attackEffect, effectPosition, transform.rotation);
+
+            Destroy(effect, 2f);
+        }
+
+        Debug.Log("Attack performed!");
     }
 
     void OnTriggerEnter(Collider other)
@@ -73,4 +93,18 @@ public class PlayerControls : MonoBehaviour
         }
     }
 
+    public void OnAttack(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            PerformMeleeAttack();
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Vector3 attackPosition = transform.position + transform.forward * (attackRange * 0.5f);
+        Gizmos.DrawWireSphere(attackPosition, attackRange);
+    }
 }
